a8d33d0cad75aa285bb7e78da94fc828f160f063
Hamza Khairi <hkhairi@e1r6p7.1337.ma>
2025-02-27 09:55:07 +0000

    update

--- client.c
+++ client.c
@@ -1,24 +1,64 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkhairi <hkhairi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/01 14:33:51 by hkhairi           #+#    #+#             */
-/*   Updated: 2025/01/01 19:26:27 by hkhairi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#include "header.h"
+volatile int ack_received = 0;
 
-int	main(int argc, char **argv)
+void handle_ack(int sig)
 {
-	int	pid;
+    (void)sig;
+    ack_received = 1;
+}
 
-	if (argc != 3)
-		return (-1);
-	pid = ft_atoi(argv[1]);
-	convert_to_binary(argv[2], pid);
-	return (0);
+void send_char(pid_t server_pid, char c)
+{
+    int bit;
+
+    for (bit = 7; bit >= 0; bit--)
+    {
+        ack_received = 0;
+        if ((c >> bit) & 1)
+            kill(server_pid, SIGUSR1);
+        else
+            kill(server_pid, SIGUSR2);
+
+        while (!ack_received)
+            usleep(100);
+    }
+}
+
+void send_message(pid_t server_pid, char *message)
+{
+    while (*message)
+    {
+        send_char(server_pid, *message);
+        message++;
+    }
+    send_char(server_pid, '\0');
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s <server_pid> <message>\n", argv[0]);
+        return (1);
+    }
+
+    pid_t server_pid = atoi(argv[1]);
+    if (server_pid <= 0)
+    {
+        printf("Invalid PID\n");
+        return (1);
+    }
+
+    struct sigaction sa;
+    sa.sa_handler = handle_ack;
+    sa.sa_flags = 0;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
+
+    send_message(server_pid, argv[2]);
+    return (0);
 }
--- server.c
+++ server.c
@@ -1,41 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkhairi <hkhairi@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/01 14:33:49 by hkhairi           #+#    #+#             */
-/*   Updated: 2025/01/02 21:29:59 by hkhairi          ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
 
-#include "header.h"  
-
-void	handel_binary(int sig)
+void handle_signal(int sig, siginfo_t *info, void *context)
 {
-	static int	i = 0;
-	static char	c[8];
+    static int bit = 0;
+    static unsigned char character = 0;
+    static  pid_t pid;
 
-	if (sig == SIGUSR1)
-		c[i] = '1';
-	else if (sig == SIGUSR2)
-		c[i] = '0';
-	i++;
-	if (i == 8)
-	{
-		i = 0;
-		convert_to_txt(c);
-	}
+    (void)context;
+    if(pid != info->si_pid)
+    {
+        pid = info->si_pid;
+        character = 0;
+        bit = 0;
+    }
+    if (sig == SIGUSR1)
+        character |= (1 << (7 - bit));
+
+    bit++;
+
+    if (bit == 8)
+    {
+        write(1, &character, 1);
+        if (character == '\0')
+            write(1, "\n", 1);
+        bit = 0;
+        character = 0;
+    }
+
+    kill(info->si_pid, SIGUSR1);
 }
 
-int	main(void)
+int main(void)
 {
-	ft_putnbr(getpid());
-	write(1, "\n", 1);
-	signal(SIGUSR1, handel_binary);
-	signal(SIGUSR2, handel_binary);
-	while (1)
-		sleep(1);
-	return (0);
+    struct sigaction sa;
+
+    sa.sa_sigaction = handle_signal;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+
+    printf("Server PID: %d\n", getpid());
+
+    if (sigaction(SIGUSR1, &sa, NULL) == -1 || sigaction(SIGUSR2, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        return (1);
+    }
+
+    while (1)
+        pause();
 }
